de3a486227506f083e789d7985660ca3d24a336b
bibek <crazy764gaming@gmail.com>
2026-09-08 12:41:49 -0700

    Import Link used by Dashboard

--- web_app/frontend/src/pages/Dashboard/Dashboard.tsx
+++ web_app/frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { RefreshCw, Activity, ShieldCheck, Wifi, Radio } from 'lucide-react';
 import { LiveMap } from '../../components/dashboard/LiveMap';
 import { RobotStatus } from '../../components/dashboard/RobotStatus';
